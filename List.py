9bab759127358c832ea24f68099aec877d7f1011
chaitanyapoighal <110621512+chaitanyapoighal@users.noreply.github.com>
2024-01-12 15:30:43 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,10 @@
+my_list = [1, 2, 3, 4, 5]
+length = len(my_list)
+print(length)
+my_list.extend([7,8,9])
+print(my_list)
+my_list.insert(3,10)
+print(my_list)
+exists=10 in my_list
+print(exists)
+print(my_list.sort())
